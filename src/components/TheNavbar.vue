a8bcdeceb1c36a4d3c90afcb5a4a497f2481c8e4
matiascabrejos <matias.cabrejos@hotmail.com>
2022-07-02 22:39:31 -0300

    Working on the style of the navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,69 +1,71 @@
 <template>
   <div>
-    <nav class="wrapper py-6 bg-gray-800">
-      <div class="px-10 flex justify-between items-center">
-        <div class="logo">
-          <router-link to="/">
-            <h1 class="navbar-text-logo">
-              <span class="font-bold">mAt</span>deV.
-            </h1>
+    <body class="bg-gray-800">
+      <nav class="wrapper py-6 bg-gray-800">
+        <div class="px-10 flex justify-between items-center">
+          <div class="logo">
+            <router-link to="/">
+              <h1 class="navbar-text-logo">
+                <span class="font-bold">mAt</span>deV.
+              </h1>
+            </router-link>
+          </div>
+
+          <div class="navbar hidden md:block">
+            <router-link class="nav-link" to="/">Home</router-link>
+            <router-link class="nav-link" to="/create-blog"
+              >Create Blog</router-link
+            >
+          </div>
+
+          <div class="ml-3 flex md:hidden">
+            <!-- Mobile menu button -->
+            <button @click="toggle" class="flex-center rounded-md">
+              <svg
+                :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+              <svg
+                :class="[isOpen ? 'block' : 'hidden', 'h-6 w-6']"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
+        <div class="px-2 pt-2 pb-3 sm:px-3">
+          <router-link to="/" class="mobile-link focus:outline-none"
+            >Home
+          </router-link>
+          <router-link to="/create-blog" class="mt-1 mobile-link focus:outline-none"
+            >Create Blog
           </router-link>
         </div>
-
-        <div class="navbar hidden md:block">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/create-blog"
-            >Create Blog</router-link
-          >
-        </div>
-
-        <div class="ml-3 flex md:hidden">
-          <!-- Mobile menu button -->
-          <button @click="toggle" class="flex-center rounded-md">
-            <svg
-              :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-            <svg
-              :class="[isOpen ? 'block' : 'hidden', 'h-6 w-6']"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          </button>
-        </div>
       </div>
-    </nav>
-    <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
-      <div class="px-2 pt-2 pb-3 sm:px-3">
-        <router-link to="/" class="mobile-link focus:outline-none"
-          >Home
-        </router-link>
-        <router-link to="about" class="mt-1 mobile-link focus:outline-none"
-          >About
-        </router-link>
-      </div>
-    </div>
+    </body>
   </div>
 </template>
 
